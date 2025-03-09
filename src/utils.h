1a2fe9840291afe50c49928d4a0ac7e4a2f2dbf0
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2025-03-10 05:14:29 +0530

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -4,8 +4,6 @@
 #include <array>
 #include <cmath>
 #include <vector>
-#include <chrono>
-
 
 const std::array<int,4> gmumu = {1,-1,-1,-1};
 const std::array<int,4> t_vector = {1,0,0,0};
@@ -16,64 +14,29 @@ const double hbarC = 1. / 5.067728853; //=0.197 Gev*fm
 const double PI = std::acos(-1);
 
 int levi(int i, int j, int k, int l);
+int statistics(double spin);
 std::vector<double> linspace(double min, double max, int size);
 std::tuple<std::vector<double>,std::vector<double>,std::vector<double>,std::vector<double>> file_to_4_column(std::string filename, int f_column);
 
-//class for interpolation
-class interpolator{
-    private:
-    public:
-    std::vector<double> x_, y_, z_, f_; //x,y,z vector of a table of the values x. All quantities have the same lenght
-    int nx, ny, nz;
-    double xmaxG;
-    double ymaxG;
-    double zmaxG;
-    double xminG;
-    double yminG;
-    double zminG;
-    double dxG;
-    double dyG;
-    double dzG;
-        interpolator(std::vector<double> x, std::vector<double> y, std::vector<double> z, std::vector<double> f);
-        interpolator(std::string filename, int f_column);
-        ~interpolator();
+double dotProduct(const std::array<double, 3>& a, const std::array<double, 3>& b);
 
-        double trilinear_interpolation(double x, double y, double z);
-        double trilinear_interpol(double x, double y, double z);
-        double f_index(int ix, int iy, int iz);
-
+struct NonZeroLevi {
+    int mu, nu, rh, sg;
+    double value;
 };
 
-constexpr std::array<std::array<int, 5>, 24> non_zero_levi()
-    {
-        /// List of arrays of indices for which the Levi Civita symbol is non-zero, 
-        //the fifth entry being the value of the symbol
-        return {{
-            {0, 1, 2, 3, 1},
-            {0, 2, 1, 3, -1},
-            {0, 3, 1, 2, 1},
-            {0, 1, 3, 2, -1},
-            {0, 3, 2, 1, -1},
-            {0, 2, 3, 1, 1},
-            {1, 0, 2, 3, -1},
-            {1, 2, 0, 3, 1},
-            {1, 3, 0, 2, -1},
-            {1, 0, 3, 2, 1},
-            {1, 2, 3, 0, -1},
-            {1, 3, 2, 0, 1},
-            {2, 0, 1, 3, 1},
-            {2, 1, 0, 3, -1},
-            {2, 3, 0, 1, 1},
-            {2, 0, 3, 1, -1},
-            {2, 1, 3, 0, 1},
-            {2, 3, 1, 0, -1},
-            {3, 0, 1, 2, -1},
-            {3, 1, 0, 2, 1},
-            {3, 2, 0, 1, -1},
-            {3, 0, 2, 1, 1},
-            {3, 1, 2, 0, -1},
-            {3, 2, 1, 0, 1},
-        }};
-    }
+std::vector<NonZeroLevi> getNonZeroLevi();
+
+class trilinearInterpolator {
+private:
+    int npT, nphi, ny;
+    double pTmin, phimin, ymin, pTmax, phimax, ymax, dpT, dphi, dy;
+    std::vector<double> pT_grid, phi_grid, y_grid;
+
+public:
+    trilinearInterpolator(const std::vector<double>& pG_, const std::vector<double>& phG_, const std::vector<double>& yG_);
+    double trilinearInterpolation(const double pT, const double phi, const double y_rap, const std::vector<std::vector<std::vector<double>>>& P_primary);
+    std::vector<double> trilinearInterpolation(const double pT, const double phi, const double y_rap, const std::vector<std::vector<std::vector<std::vector<double>>>>& P_primary);
+};
 
 #endif

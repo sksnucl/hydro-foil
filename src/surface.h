2a258ce9db44d392133b34f4765aa316af551763
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2025-03-10 05:15:31 +0530

    Update surface.h

--- src/surface.h
+++ src/surface.h
@@ -8,18 +8,13 @@
 //meant to read a the beta.dat file from the output of vhlle (polarization branches)
 
 struct element {
-    double tau=0, x=0, y=0, eta=0;
-    std::array<double,4> u={0};
+    double tau=0, x=0, y=0, eta=0, ed=0, p=0;
+    std::array<double,4> u={1,0,0,0};
     std::array<double,4> dsigma={0};
     double T=0, mub=0, muq=0, mus=0; //GeV
     std::array<std::array<double,4>,4> dbeta={0};
-    // element() : u(4, 0), dsigma(4, 0), dbeta(4, std::array<double>(4, 0)){};
-    void print();
 };
 
-void read_hypersrface(std::string filename, std::vector<element> &hypersurface);
-
-element new_dbeta(element surf_old, int tag);
-std::array<std::vector<element>,5> components_freeze_out(std::vector<element> &freeze_out_sup);
+void read_hypersurface(std::string filename, std::vector<element> &hypersurface);
 
 #endif
